97405aa130499d80e6b9e866081d04646ac13f90
Jahanzeb Ahmed <151543326+Jahanzeb-git@users.noreply.github.com>
2025-01-05 01:30:39 +0500

    Update latex.tsx

--- src/components/Chat/latex.tsx
+++ src/components/Chat/latex.tsx
@@ -3,14 +3,17 @@ import React from 'react';
 const markdownContent = `
 Here is a simple LaTeX equation:
 
-\( y_i = \beta_0 + \beta_1 x_i + \epsilon_i \)
+\\[ E = mc^2 \\]
 `;
 
 export default function App() {
   return (
     <div>
-      <div id="math" dangerouslySetInnerHTML={{ __html: markdownContent.replace(/\\\[(.*?)\\\]/g, '<span class="math">$1</span>') }}></div>
+      <div id="math" className="math-content">
+        {markdownContent.replace(/\\\[(.*?)\\\]/g, '<span class="math">$1</span>')}
+      </div>
     </div>
   );
 }
+
 
