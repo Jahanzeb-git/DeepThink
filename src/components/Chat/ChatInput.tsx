8dc465b6803a6c93f0e0c6ed3e902631c8af4e51
Jahanzeb Ahmed <151543326+Jahanzeb-git@users.noreply.github.com>
2025-01-27 19:31:07 +0500

    Update ChatInput.tsx

--- src/components/Chat/ChatInput.tsx
+++ src/components/Chat/ChatInput.tsx
@@ -67,7 +67,7 @@ export default function ChatInput({
   return (
     <form
       onSubmit={handleSubmit}
-      className="relative bg-[#584b49] dark:bg-gray-300 rounded-3xl border border-[#584b49] dark:border-gray-300 p-4"
+      className="relative bg-gray-700 dark:bg-gray-300 rounded-3xl border border-gray-700 dark:border-gray-300 p-4"
     >
       <div className="relative">
         <textarea
