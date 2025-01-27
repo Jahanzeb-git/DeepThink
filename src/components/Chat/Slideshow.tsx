c9d48b9a8a4c2c1072514934336dd16825299c9c
Jahanzeb Ahmed <151543326+Jahanzeb-git@users.noreply.github.com>
2025-01-27 23:12:09 +0500

    Update Slideshow.tsx

--- src/components/Chat/Slideshow.tsx
+++ src/components/Chat/Slideshow.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react';
 import { motion, useMotionValue } from 'framer-motion';
-import Image1 from './image1.jpg';
+import Image1 from './image1.png';
 import Image2 from './image2.jpg';
 import Image3 from './image3.jpg';
 import Image4 from './image4.jpg';
