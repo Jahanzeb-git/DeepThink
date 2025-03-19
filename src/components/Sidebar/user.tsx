a21502514e5b51c56b87700a3c6b5ec06659b931
Jahanzeb Ahmed <151543326+Jahanzeb-git@users.noreply.github.com>
2025-03-19 16:42:48 +0500

    Update user.tsx
    updated to add Setting UI

--- src/components/Sidebar/user.tsx
+++ src/components/Sidebar/user.tsx
@@ -12,6 +12,7 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { ThemeToggle } from '../ThemeToggle';
 import LoadingModal from './loading';
 import LoadingModal2 from './loading2';
+import Settings from './settings';
 
 interface TabProps {
     isActive: boolean;
@@ -468,6 +469,11 @@ const ProfileModal: React.FC<ProfileModalProps> = ({ setShowProfile, isDark, tog
                                         </div>
                                     </div>
                                     <ThemeToggle isDark={isDark} toggleTheme={toggleTheme} />
+                                </div>
+
+                                {/* Behavioral Settings */}
+                                <div className="p-4 rounded-xl bg-gray-800/50 dark:bg-gray-200/50">
+                                    <Settings />
                                 </div>
                             </div>
                         </div>
